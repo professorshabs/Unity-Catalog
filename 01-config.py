65896c27b6f768c500fe63c3b0f227a5c635857d
professorshabs@gmail.com <professorshabs@gmail.com>
2023-11-13 09:37:48 +0000

    add gold and all the unit testing

--- 01-config.py
+++ 01-config.py
@@ -1,7 +1,14 @@
 # Databricks notebook source
 class Config():    
     def __init__(self):      
-        self.base_dir_data = spark.sql("describe external location `data_zone`").select("url").collect()[0][0]
-        self.base_dir_checkpoint = spark.sql("describe external location `checkpoint`").select("url").collect()[0][0]
+        self.base_dir = spark.sql("describe external location `health_data_landing_zone`").select("url").collect()[0][0]
+        self.base_dir_data = f"{self.base_dir}data"
+        self.base_dir_checkpoint = f"{self.base_dir}checkpoints"
         self.db_name = "sbit_db"
         self.maxFilesPerTrigger = 1000
+
+
+if __name__ == "__main__":
+    conf = Config()
+    print(conf.base_dir_data)
+    print(conf.base_dir_checkpoint)
